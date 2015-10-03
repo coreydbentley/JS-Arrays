d56fe45b89e394a83dedf2b3b4cf98fd3c575aba
coreydbentley <coreydbentley@gmail.com>
2015-10-02 22:55:56 -0600

    finished JS-Arrays

--- arraysPractice.js
+++ arraysPractice.js
@@ -365,7 +365,8 @@ of Data is to have an Array full of objects. */
 
 //Create an empty array called users.
 
-  //Code Here
+  var users = [];
+
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
@@ -378,7 +379,28 @@ var user1 = {
     username: 'infiniateLoop'
 };
 
-//Your Code Here
+var user2 = {
+    name: 'Corey Bentley',
+    email: 'coreybentley@gmail.com',
+    password: 'thePassword',
+    username: 'cbentley'
+};
+
+var user3 = {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: 'myPassword',
+    username: 'jdoe'
+};
+
+users.push(user1, user2, user3);
+
+
+
+
+
+
+  )
 
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
@@ -388,7 +410,12 @@ and those objects contain properties about the specific person you follow.*/
 objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 Once you find the particular index he's located in, delete him from the array.*/
 
-  //Code Here
+  for(i=0; i < users.length; i++){
+    if( users[i].email === 'tylermcginnis33@gmail.com'){
+        users.splice(i, 1);
+    }
+  }
+
 
 //The activity we just did is very much how data works in 'the real world'.
 
